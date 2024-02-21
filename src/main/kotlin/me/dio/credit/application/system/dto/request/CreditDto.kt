ed7d0142aee53c8913e8bf9d453a6ef79098dc6a
baltar9 <koonn8657@gmail.com>
2024-02-21 16:41:25 -0300

    finalcommit

--- src/main/kotlin/me/dio/credit/application/system/dto/request/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/request/CreditDto.kt
@@ -1,8 +1,7 @@
-package me.dio.credit.application.system.dto
+package me.dio.credit.application.system.dto.request
 
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.entity.Customer
@@ -13,7 +12,7 @@ data class creditDto(
     @field:NotNull(message = "Invalid Input") val creditValue: BigDecimal,
     @field:Future(message = "Invalid Input")val dayFirstInstallment: LocalDate,
     @field:Min(value = 20 , message = "Invalid number") val numberOfInstallments: Int,
-    @field:NotEmpty(message = "Invalid Input") val customerId: Long
+    @field:NotNull(message = "Invalid Input") val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
